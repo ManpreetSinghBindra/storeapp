32373d2d2e7222e6975ee60218bc5b1bf7372e50
ManpreetSinghBindra <41113278+ManpreetSinghBindra@users.noreply.github.com>
2020-05-06 16:31:28 +0530

    Added comments

--- src/main/java/com/mphasis/controller/ProductRestController.java
+++ src/main/java/com/mphasis/controller/ProductRestController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mphasis.domain.Product;
 import com.mphasis.service.IProductService;
 
+// Product Restful WebServices
+
 @RestController
 @Scope("request")
 public class ProductRestController {
